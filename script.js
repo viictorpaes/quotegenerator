faefe836df44b12962c4984271116f0d258259e2
Vepe <victorjosepaes@gmail.com>
2023-10-23 22:53:07 -0300

    adding a loading animation

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
 
+//show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+};
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 //Show New Quote
 function newQuote(){
+    loading();
     // Pick a random quote from a apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //check if Author field is blank and raplace it with 'Unknown'
@@ -24,12 +38,14 @@ function newQuote(){
    }else{
     quoteText.classList.remove('long-quote');
    }
-
+   // set quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API  
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
